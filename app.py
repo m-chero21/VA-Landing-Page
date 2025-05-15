0ff870c4ad8d6b92748e1526af59e34b8b3763a7
m-chero21 <michelle.cherotich21@gmail.com>
2025-05-15 22:26:45 +0300

    Initial commit

--- app.py
+++ app.py
@@ -22,21 +22,3 @@ if __name__ == "__main__":
 
 
 
-import jwt
-import time
-
-@app.route("/dashboard")
-def dashboard():
-    METABASE_SITE_URL = "http://localhost:3000"
-    METABASE_SECRET_KEY = "4d50ef39e0169eacb2e3c224e8cb48ffea909961bc7a056dcf317a1ee0c0d727"
-
-    payload = {
-        "resource": {"dashboard": 2},
-        "params": {},
-        "exp": round(time.time()) + (60 * 10)  # 10 minute expiry
-    }
-
-    token = jwt.encode(payload, METABASE_SECRET_KEY, algorithm="HS256")
-    iframeUrl = f"{METABASE_SITE_URL}/embed/dashboard/{token}#bordered=true&titled=true"
-
-    return render_template("dashboard.html", iframeUrl=iframeUrl)
